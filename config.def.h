6e1c05e074c3491f76dc144f5cf4fd268037cccf
Hannes Gräuler <hgraeule@uos.de>
2010-12-28 22:29:10 +0100

    dwm-5.8.2-bstack-alt.diff

--- config.def.h
+++ config.def.h
@@ -31,6 +31,8 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+ 	{ "TTT",      bstack },
+ 	{ "===",      bstackhoriz },
 };
 
 /* key definitions */
